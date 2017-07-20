488f078be55f0efe64a30fbd618e624738cdc94b
nathanye <khalil54781@gmail.com>
2017-07-20 21:29:25 +0800

    add date UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require nested_form_fields
-//= require_tree .
 //= require select2
+//= require nested_form_fields
+//= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN
+//= require_tree .
