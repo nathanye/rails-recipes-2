01b5b5c33e3ab87188923fb46e0837f67be1c4a2
nathanye <khalil54781@gmail.com>
2017-07-21 16:16:50 +0800

    diy validate error message

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -18,6 +18,8 @@ class Registration < ApplicationRecord
   validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
   validates_presence_of :name, :email, :cellphone, :bio, :if => :should_validate_all_data?
 
+  validate :check_event_status, :on => :create
+
   protected
 
   def generate_uuid
@@ -30,5 +32,11 @@ class Registration < ApplicationRecord
 
   def should_validate_all_data?
     current_step == 3 || status == "confirmed" # 做到第三步，或最后状态是 confirmed 时需要验证
+  end
+
+  def check_event_status
+    if self.event.status == "draft"
+      errors.add(:base, "活动尚未开放报名")
+    end
   end
 end
